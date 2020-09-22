5c17b0f5d58caf7aee498eb0c54ef94c356adbeb
tzahidavid014@gmail.com <your_email@gmool.mom>
2020-09-22 11:52:37 +0300

    fixing

--- dist/logic.js
+++ dist/logic.js
@@ -3,22 +3,21 @@ class Model {
         this.cityData = []
     }
 
-    momentDateFormat (city){
-       return moment(city.updatedAt).format('LLLL')
+    momentDateFormat(city) {
+        return moment(city.updatedAt).format('LLLL')
     }
 
     async getDataFromDB() {
-        const fatch = await $.get("/cities")
-        fatch.forEach(a => {
-            a.updatedAt = moment(a.updatedAt).format('LLLL')
-            this.cityData.push(a)
-            console.log(a)
+        const cities = await $.get("/cities")
+        cities.forEach(city => {
+            city.updatedAt = moment(city.updatedAt).format('LLLL')
+            this.cityData.push(city)
         });
     }
-   
+
     async getCityData(cityName, lat, lon) {
-        if (!cityName) {
-            return alert ("try again")
+        if (!cityName&&!lat&&!lon) {
+            return alert("try again")
         }
         let exist = false
         this.cityData.forEach(a => {
@@ -29,22 +28,15 @@ class Model {
         if (exist) {
             return alert("City already exist, you can update your data")
         }
-        if (lat && lon) {
-            const fatch = await $.get(`/city/${cityName}?lat=${lat}&lon=${lon}`)
-            this.cityData.push({ ...fatch, saved: false })
-            console.log(fatch)
-        }
-        else{
-            const fatch = await $.get(`/city/${cityName}`)
-            this.cityData.push({ ...fatch, saved: false })
-            console.log(fatch)
-        }
+        const params = lat && lon ? `?lat=${lat}&lon=${lon}` : ''
+        const fatch = await $.get(`/city/${cityName}${params}`)
+        this.cityData.push({ ...fatch, saved: false })
     }
 
 
     async getPosition() {
         return new Promise(function (resolve, reject) {
-            if(navigator.geolocation){
+            if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(resolve, reject);
             }
         });
@@ -52,16 +44,15 @@ class Model {
 
     async getCurrentCity() {
         let position = await this.getPosition()
-        await this.getCityData("dummyCity", position.coords.latitude, position.coords.longitude)
+        await this.getCityData(null, position.coords.latitude, position.coords.longitude)
     }
-    
-    
+
+
     async saveCity(city) {
         const findCity = this.cityData.find(a => a.name === city)
         const findCityID = this.cityData.findIndex(a => a.name === city)
-        console.log(findCityID)
         await $.post("/city", findCity)
-        this.cityData[findCityID].saved = true
+        findCity.saved = true
     }
 
     async removeCity(cityName) {
@@ -72,9 +63,9 @@ class Model {
             url: `/city/${CityNameToDelete.name}`,
             success: (data) => {
                 this.cityData.data = data // don't need this line, data return that we deleted this city
-                const findCityID = this.cityData.findIndex(a => a.name === cityName)
-                console.log(findCityID)
-                this.cityData[findCityID].saved = false
+                const cityIndex = this.cityData.findIndex(a => a.name === cityName)
+                console.log(cityIndex)
+                this.cityData[cityIndex].saved = false
             },
             error: function (xhr, text, error) {
                 console.log("city not on DB")
@@ -85,7 +76,7 @@ class Model {
     async updateCity(cityName) {
         const cityToUpdate = this.cityData.find(a => a.name === cityName)
         console.log(cityToUpdate)
-       const data = await $.ajax({
+        const data = await $.ajax({
             method: "PUT",
             url: `/city/${cityToUpdate.name}`
         })
@@ -98,16 +89,15 @@ class Model {
             }
         }
     }
-    
-     async checkLastUpadate () {
+
+    async checkLastUpadate() {
         if (this.cityData.length > 0) {
-            console.log("tzahi")
-            for (let a of this.cityData){
+            for (let a of this.cityData) {
                 let newTime = new moment()
                 const diff = moment.duration(newTime.diff(a.updatedAt))._data.hours
                 console.log(diff)
                 if (diff > 3) {
-                     await this.updateCity(a.name)
+                    await this.updateCity(a.name)
                 }
             }
         }
--- dist/main.js
+++ dist/main.js
@@ -5,12 +5,10 @@ const loadPage = async function () {
     await model.getDataFromDB()
     await model.getCurrentCity()
     await model.checkLastUpadate()
-    console.log(model.cityData.length)
     renderer.renderAll(model.cityData)
 }
 loadPage()
 const current = async function () {
-    debugger
     await model.getCityData()
 }
 
@@ -26,24 +24,18 @@ $("#city-btn").on("click", function () {
 })
 
 $(".container").on("click", "#saved-btn", async function () {
-    console.log("hello")
     const cityName = $(this).closest(".city").find("#name").text()
-    console.log(cityName)
     await model.saveCity(cityName)
     renderer.renderAll(model.cityData)
 })
 
 $(".container").on("click", "#removed-btn", async function () {
-    console.log("hey")
     const cityName = $(this).closest(".city").find("#name").text()
-    console.log(cityName)
     await model.removeCity(cityName)
     renderer.renderAll(model.cityData)
 })
 $(".container").on("click", "#updated-btn", async function () {
-    console.log("updated")
     const cityName = $(this).closest(".city").find("#name").text()
-    console.log(cityName)
     await model.updateCity(cityName)
     renderer.renderAll(model.cityData)
 })
